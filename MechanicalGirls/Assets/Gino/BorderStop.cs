2a66f2d3418452534bd632df3b6bcc953cf221eb
Rutger1111 <90682560+Rutger1111@users.noreply.github.com>
2024-05-06 10:57:26 +0200

    fix

--- MechanicalGirls/Assets/Gino/BorderStop.cs
+++ MechanicalGirls/Assets/Gino/BorderStop.cs
@@ -19,22 +19,25 @@ public class BorderStop : MonoBehaviour
     {
     }
     public void WaitPlease(){
-        if (Player1.position.x >= borderRight.position.x)
+        if (Player1.position.x >= borderRight.position.x && Player1Move.horizontalInput == 1)
         {
             Player1Move.horizontalInput = 0;
         }
-        if (Player1.position.x <= borderLeft.position.x)
+        if (Player1.position.x <= borderLeft.position.x && Player1Move.horizontalInput == -1)
         {
             Player1Move.horizontalInput = 0;
         }
-        if (Player2.position.x >= borderRight.position.x)
+        if (Player2.position.x >= borderRight.position.x && Player2Move.horizontalInput == 1)
         {
             Player2Move.horizontalInput = 0;
         }
-        if (Player2.position.x <= borderLeft.position.x)
+        if (Player2.position.x <= borderLeft.position.x && Player2Move.horizontalInput == -1)
         {
             Player2Move.horizontalInput = 0;
         }
+    }
+    public void WaitPleaseXLeft(){
+
     }
 }
 
